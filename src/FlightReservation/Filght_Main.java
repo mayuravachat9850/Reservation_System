b2969865bab943107f945c70861e1ee1faba7c7a
Mayur Avachat <87423951+mayuravachat9850@users.noreply.github.com>
2021-07-24 13:16:34 +0530

    Created objects...

--- src/FlightReservation/Filght_Main.java
+++ src/FlightReservation/Filght_Main.java
@@ -1,4 +1,8 @@
 package FlightReservation;
 
 public class Filght_Main {
+    Passenger passenger = new Passenger();
+    Flight flight = new Flight();
+    Regular_Ticket  regularTicket = new Regular_Ticket();
+    Tourist_Ticket touristTicket = new Tourist_Ticket();
 }
