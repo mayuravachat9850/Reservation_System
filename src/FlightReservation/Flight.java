88365097539213f17c12772b1a9d4585562c1192
Mayur Avachat <87423951+mayuravachat9850@users.noreply.github.com>
2021-07-24 13:23:29 +0530

    implements all the code and relationship Checkpoint 3

--- src/FlightReservation/Flight.java
+++ src/FlightReservation/Flight.java
@@ -1,4 +1,26 @@
 package FlightReservation;
 
 public class Flight {
+    private String flightNumber;
+    private String airline;
+    private int capacity;
+    private int bookedseats;
+
+    public Flight(String flightNumber, String airline, int capacity, int bookedseats) {
+        this.flightNumber = flightNumber;
+        this.airline = airline;
+        this.capacity = capacity;
+        this.bookedseats = bookedseats;
+    }
+    public void incrementBookingCounter(){
+        this.bookedseats++;
+    }
+
+    public boolean checkAvalibility(){
+        if(this.bookedseats < capacity)
+            return true;
+        else
+            return false;
+    }
 }
+
